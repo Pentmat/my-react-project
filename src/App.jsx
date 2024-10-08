9024248926f31169693829ab4962466ae6d14c4a
Pentmat <156056783+Pentmat@users.noreply.github.com>
2024-10-08 15:05:36 +0300

    Improved component system

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,13 @@
 // App.js
-import { useState } from 'react';
 import RyzenLogo from './assets/ryzen.svg';
 import './App.css';
-import { Header, ProductForm, OrderInfo } from './ComponentUI';
+import ComponentUI from './ComponentUI'; // Import the whole ComponentUI
 
 function App() {
-  const [orders, setOrders] = useState([]);
-
-  const handleAddToOrder = (productName, productPrice, quantity) => {
-    setOrders((prevOrders) => {
-      
-      const existingOrderIndex = prevOrders.findIndex(
-        (order) => order.productName === productName
-      );
-  
-      if (existingOrderIndex === -1) {
-        
-        const newOrder = { productName, productPrice, quantity };
-        return [...prevOrders, newOrder];
-      } else {
-        
-        return prevOrders;
-      }
-    });
-  };
-
-  const handleIncreaseQuantity = (index) => {
-    const newOrders = [...orders];
-    newOrders[index].quantity += 1;
-    setOrders(newOrders);
-  };
-
-  const handleDecreaseQuantity = (index) => {
-    const newOrders = [...orders];
-    if (newOrders[index].quantity > 1) {
-      newOrders[index].quantity -= 1;
-      setOrders(newOrders);
-    }
-  };
-
-  const handleDeleteOrder = (index) => {
-    const newOrders = orders.filter((_, i) => i !== index);
-    setOrders(newOrders);
-  };
-
   return (
     <div className="App">
-      <Header title="Shop for ryzen" image={RyzenLogo} />
-      <ProductForm onAddToOrder={handleAddToOrder} />
-      {orders.length > 0 && (
-        <OrderInfo 
-          orders={orders} 
-          onIncreaseQuantity={handleIncreaseQuantity} 
-          onDecreaseQuantity={handleDecreaseQuantity} 
-          onDeleteOrder={handleDeleteOrder} 
-        />
-      )}
+      <Header title="Shop for Ryzen" image={RyzenLogo} />
+      <ComponentUI /> {/* ComponentUI will handle all the product order logic */}
     </div>
   );
 }
