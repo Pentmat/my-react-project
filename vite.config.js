c66277d081adabb5e47de5967e850831f1c5887f
Pentmat <156056783+Pentmat@users.noreply.github.com>
2024-10-15 11:06:40 +0300

    I just created a mess

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<my-react-project>/',  // Set this to your repository name
+  base: '/<my-react-project>/', 
 });
