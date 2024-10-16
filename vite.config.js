a8e587a3c5b72077d8188cceb6a5c3b0bfe1f328
Pentmat <156056783+Pentmat@users.noreply.github.com>
2024-10-16 10:04:17 +0300

    fixed?

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/my-react-project/',
   plugins: [react()],
-  base: '/my-react-project/', 
 });
